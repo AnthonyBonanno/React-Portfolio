046bd88e13e37ab0028b731e2d886ff3dda0e237
AnthonyBonanno <bonant12@gmail.com>
2025-01-21 23:58:36 +1100

    adding a _redirect file

--- react-portfolio/src/pages/Contact.jsx
+++ react-portfolio/src/pages/Contact.jsx
@@ -1,50 +1,7 @@
 import Form from "react-bootstrap/Form";
-import { useState } from "react";
-import { validateEmail } from "../../utils/helpers";
-import Container from "react-bootstrap/esm/Container";
 
-function Contact() {
-  // const [contactForm, setContactForm] = useState({
-  //   name: "",
-  //   email: "",
-  //   message: "",
-  // });
-
-  // const [error, setError] = useState("");
-  // const { name, email, message } = contactForm;
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   if (!error) {
-  //     console.log("form submitted", contactForm);
-  //   }
-  // };
-
-  // const handleChange = (e) => {
-  //   if (e.target.name === "email") {
-  //     const validEmail = validateEmail(e.target.name);
-  //     if (!validEmail) {
-  //       setErrorMessage("Invalid Email.");
-  //     } else {
-  //       setErrorMessage("");
-  //     }
-  //   } else {
-  //     if (!e.target.value.length) {
-  //       setErrorMessage(`${e.target.name} is required.`);
-  //     } else {
-  //       setErrorMessage("");
-  //     }
-  //   }
-  //   if (!setErrorMessage) {
-  //     setContactForm({ ...contactForm, [e.target.name]: e.target.value });
-  //     console.log("Handle Form", contactForm);
-  //   }
-  // }
-
+export default function Contact() {
   return (
-    // <Container>
-
-    // </Container>
     <Form>
       <h1>Contact Me!</h1>
       <Form.Group className="mb-3">
@@ -63,5 +20,3 @@ function Contact() {
     </Form>
   );
 }
-
-export default Contact();
--- react-portfolio/src/pages/Portfolio.jsx
+++ react-portfolio/src/pages/Portfolio.jsx
@@ -9,26 +9,31 @@ const Portfolio = () => {
       title: "Logo Generator",
       description: "An app that can create a small logo with text and colour.",
       repo: "https://github.com/AnthonyBonanno/Logo-Generator",
+      imageSrc: "/images/sample.PNG",
     },
     {
       title: "Weather Application",
       description: "A web app that uses third-party APIs to display weather.",
       repo: "https://github.com/AnthonyBonanno/Weather-App",
+      imageSrc: "/images/sample.PNG",
     },
     {
       title: "E-Commerce Back End",
       description: "Back end for an e-commerce web app that uses Sequelize.",
       repo: "https://github.com/AnthonyBonanno/E-Commerce-Back-End",
+      imageSrc: "/images/sample.PNG",
     },
     {
       title: "Employee Tracker CLI",
       description: "A CLI application for managing your employees.",
       repo: "https://github.com/eddiespag-hetti/EmployeeDB_CLI",
+      imageSrc: "/images/sample.PNG",
     },
     {
       title: "Readme Generator",
       description: "Generates readme file based off of user input.",
       repo: "https://github.com/AnthonyBonanno/Readme-Generator",
+      imageSrc: "/images/sample.PNG",
     },
   ];
 
